e52a082be53d38a77679a758a6844e9939a592cc
Ken Do <xneoken@yahoo.com>
2020-03-17 21:40:24 -0700

    add production phase & terraforming test

--- main.go
+++ main.go
@@ -5,10 +5,10 @@ import (
 )
 
 type player struct {
-	res	resource
-	cards	*card
-	act	*action
-	tags	tag
+	res   resource
+	cards *card
+	act   *action
+	tags  tag
 }
 
 type resource struct {
@@ -28,18 +28,18 @@ type resource struct {
 	victoryPoint int
 }
 
-type tag struct	{
-	building	int
-	space		int
-	power		int
-	science		int
-	jovian		int
-	earth		int
-	planet		int
-	microbe		int
-	animal		int
-	city		int
-	event		int
+type tag struct {
+	building int
+	space    int
+	power    int
+	science  int
+	jovian   int
+	earth    int
+	planet   int
+	microbe  int
+	animal   int
+	city     int
+	event    int
 }
 
 type card struct {
@@ -50,42 +50,42 @@ type action struct {
 	name string
 }
 
-func (p *player) init() {
-	(*p).res.init()
+func (p *player) Init() {
+	(*p).res.Init()
 	p.cards = nil
 	p.act = nil
-	(*p).tags.init()
+	(*p).tags.Init()
 }
 
-func (r *resource) init() {
-	r.terraforming =	20
-	r.mc =			20
-	r.mcProd =		0
-	r.plant =		0
-	r.plantProd =		0
-	r.steel =		0
-	r.steelProd =		0
-	r.titanium =		0
-	r.titaniumProd =	0
-	r.energy =		0
-	r.energyProd =		0
-	r.heat =		0
-	r.heatProd =		0
-	r.victoryPoint =	0
+func (r *resource) Init() {
+	r.terraforming = 20
+	r.mc = 20
+	r.mcProd = 0
+	r.plant = 0
+	r.plantProd = 0
+	r.steel = 0
+	r.steelProd = 0
+	r.titanium = 0
+	r.titaniumProd = 0
+	r.energy = 0
+	r.energyProd = 0
+	r.heat = 0
+	r.heatProd = 0
+	r.victoryPoint = 0
 }
 
-func (t *tag) init() {
-	t.building	= 0
-	t.space		= 0
-	t.power		= 0
-	t.science	= 0
-	t.jovian	= 0
-	t.earth		= 0
-	t.planet	= 0
-	t.microbe	= 0
-	t.animal	= 0
-	t.city		= 0
-	t.event		= 0
+func (t *tag) Init() {
+	t.building = 0
+	t.space = 0
+	t.power = 0
+	t.science = 0
+	t.jovian = 0
+	t.earth = 0
+	t.planet = 0
+	t.microbe = 0
+	t.animal = 0
+	t.city = 0
+	t.event = 0
 }
 
 func (p *player) print() {
@@ -102,17 +102,35 @@ func (r *resource) print() {
 }
 
 type mars struct {
-	gen	int
-	o2	int
-	temp	int
-	ocean	int
+	gen   int
+	o2    int
+	temp  int
+	ocean int
 }
 
-func (m *mars) init() {
-	m.gen =		1
-	m.o2 =		0
-	m.temp =	0
-	m.ocean =	0
+func (m *mars) Init() {
+	m.gen = 1
+	m.o2 = 0
+	m.temp = 0
+	m.ocean = -30
+}
+
+func (r *resource) Production() {
+	r.mc = r.mc + r.mcProd
+	r.plant = r.plant + r.plantProd
+	r.steel = r.steel + r.steelProd
+	r.titanium = r.titanium + r.titaniumProd
+	r.energy = r.energy + r.energyProd
+	r.heat = r.heat + r.heatProd
+	// draw 4 new cards
+}
+
+func (m *mars) isTerraform() bool {
+	if m.o2 == 14 && m.temp == 8 && m.ocean == 9 {
+		return true
+	} else {
+		return false
+	}
 }
 
 func main() {
@@ -120,8 +138,8 @@ func main() {
 
 	var p1, p2 player
 	var m mars
-	m.init()
-	p1.init()
-	p2.init()
+	m.Init()
+	p1.Init()
+	p2.Init()
 	p1.print()
 }
